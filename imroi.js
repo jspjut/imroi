1cfbbfc623d155db17f4d4bd6dba9bb2f67b706a
Josef Spjut <jspjut@nvidia.com>
2018-10-07 23:03:04 -0400

    Fixed integer math

--- imroi.js
+++ imroi.js
@@ -23,8 +23,8 @@ function update_image() {
         zoom_img.style.backgroundPosition = 'left -' + tl_x * zoom * scale + 'px top -' + tl_y * zoom * scale + 'px';
         zoom_img.style.width = naturalWidth * scale + 'px';
         zoom_img.style.height = naturalHeight * scale + 'px';
-        zoom_img.style.gridRow = (i) / 2 + 1;
-        zoom_img.style.gridColumn = (i + 1);
+        zoom_img.style.gridRow = Math.floor(i / 2) + 1 ;
+        zoom_img.style.gridColumn = (i % 2 + 1);
 
     }
 
